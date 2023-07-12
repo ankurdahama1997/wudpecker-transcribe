44a9ac01db548214388b331309f809a8e93faa5c
joonafinland <joona@jokivuori.net>
2023-07-12 08:31:08 +0000

    Revert "Wudpecker specifc logic removed and manual lang azure option added"
    This reverts commit 9df0713096dd237500b20c1e54490ab955408b85.

--- wudpecker_transcribe/celery_config.py
+++ wudpecker_transcribe/celery_config.py
@@ -20,7 +20,7 @@ celery_app.conf.task_routes = {
 }
 
 
-def transcribe_azure_detect_language(url, uuid):
+def transcribe_azure_detect_language(url, call_uuid):
     azure_req_body = json.dumps(
         {'contentUrls': [url],
         'properties':
@@ -49,16 +49,17 @@ def transcribe_azure_detect_language(url, uuid):
                 },
             },
         'locale': "en-US",
-        'displayName': uuid})
+        'displayName': call_uuid})
     azure_key = os.getenv('AZURE_KEY')
     azure_request = requests.post('https://northeurope.api.cognitive.microsoft.com/speechtotext/v3.1/transcriptions', headers={
                                 'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': azure_key}, data=azure_req_body)
     return azure_request.text
 
 
+@celery_app.task
+def create_transcript(call_uuid, url):
+    callback = os.getenv("CREATED_CALLBACK_URL")
 
-
-def transcribe_azure_manual(url, uuid, lang):
     azure_req_body = json.dumps(
         {'contentUrls': [url],
         'properties':
@@ -72,53 +73,58 @@ def transcribe_azure_manual(url, uuid, lang):
             'wordLevelTimestampsEnabled': True,
             'punctuationMode': 'DictatedAndAutomatic',
             'profanityFilterMode': 'None',
+            "languageIdentification": {
+            "candidateLocales": [
+                "en-US",
+                "fi-FI",
+                "da-DK",
+                "fr-FR", 
+                "de-DE",
+                "pt-BR",
+                "ru-RU",
+                "es-ES",
+                "sv-SE",
+                "uk-UA"]
+                },
             },
-        'locale': lang,
-        'displayName': uuid})
+        'locale': "en-US",
+        'displayName': call_uuid})
     azure_key = os.getenv('AZURE_KEY')
     azure_request = requests.post('https://northeurope.api.cognitive.microsoft.com/speechtotext/v3.1/transcriptions', headers={
                                 'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': azure_key}, data=azure_req_body)
+    
+        
+    response_request = requests.post(callback, data=azure_request.text)
+    print(azure_request)
     return azure_request.text
 
-
 @celery_app.task
-def create_transcript(uuid, url):
-    callback = os.getenv("CREATED_CALLBACK_URL")
-    transcript = transcribe_azure_detect_language(url, uuid)
-    response_request = requests.post(callback, data=transcript)
-    return transcript
-
-
-@celery_app.task
-def create_transcript_manual(uuid, url, lang):
-    callback = os.getenv("CREATED_CALLBACK_URL")
-    transcript = transcribe_azure_manual(url, uuid, lang)
-    response_request = requests.post(callback, data=transcript)
-    return transcript
-
-
-@celery_app.task
-def deepgram_transcribe(uuid, url):
+def deepgram_transcribe(call_uuid, url):
     callback = os.getenv("DONE_CALLBACK_URL")
 
     transcript = transcribe_deepgram(url)
     formatted = parse_deepgram(transcript)
+    
+    # when there are multiple owners in the same call, update the transcript for each
+    response = requests.post(os.getenv("MULTIPLE_CALL_URL"), data={"uuid":call_uuid})
+    uuid_list = response.json()
 
-    json_file_name = uuid + '_final_.json'
-    res = boto3.resource("s3", endpoint_url='https://s3.eu-central-1.amazonaws.com')
-    s3object = res.Object(os.getenv("BUCKET_NAME"), json_file_name)
-    s3object.put(Body=(bytes(json.dumps(formatted).encode('UTF-8'))))
+    for call in uuid_list:
+        json_file_name = call + '_final_.json'
+        res = boto3.resource("s3", endpoint_url='https://s3.eu-central-1.amazonaws.com')
+        s3object = res.Object(os.getenv("BUCKET_NAME"), json_file_name)
+        s3object.put(Body=(bytes(json.dumps(formatted).encode('UTF-8'))))
 
     # Check if the meeting is coherent using coherency api
     try:
-        coherent_res = requests.get(f"{os.getenv('COHERENCY_URL')}/?azure={uuid}")
+        coherent_res = requests.get(f"{os.getenv('COHERENCY_URL')}/?azure={call_uuid}")
         if not coherent_res.json():
-            transcribe_azure_detect_language(url, uuid)
-            return json.dumps({"uuid": uuid, "status":"Incoherent"})
+            transcribe_azure_detect_language(url, call_uuid)
+            return json.dumps({"uuid": call_uuid, "status":"Incoherent"})
     except Exception as e:
         print(f"Coherency check failed: {str(e)}")
 
-    data = {"uuid": uuid, "status":"Deepgram"}
+    data = {"uuid": call_uuid, "status":"Deepgram"}
     response_request = requests.post(callback, data=data)
     return json.dumps(data)
 
@@ -151,11 +157,14 @@ def get_transcript(url):
                 return "Failed"
 
             # when there are multiple owners in the same call, update the transcript for each
-    
-            json_file_name = req_obj['displayName'] + '_final_.json'
-            res = boto3.resource("s3", endpoint_url='https://s3.eu-central-1.amazonaws.com')
-            s3object = res.Object(os.getenv("BUCKET_NAME"), json_file_name)
-            s3object.put(Body=(bytes(json.dumps(parsed).encode('UTF-8'))))
+            response = requests.post(os.getenv("MULTIPLE_CALL_URL"), data={"uuid":req_obj['displayName']})
+            uuid_list = response.json()
+
+            for call in uuid_list:
+                json_file_name = call + '_final_.json'
+                res = boto3.resource("s3", endpoint_url='https://s3.eu-central-1.amazonaws.com')
+                s3object = res.Object(os.getenv("BUCKET_NAME"), json_file_name)
+                s3object.put(Body=(bytes(json.dumps(parsed).encode('UTF-8'))))
     data = {"uuid": req_obj['displayName'], "status":status}
     if status == "Complete":
         response_request = requests.post(callback, data=data)
--- wudpecker_transcribe/main.py
+++ wudpecker_transcribe/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import PlainTextResponse
 import json
 
-from wudpecker_transcribe.celery_config import celery_app, create_transcript, get_transcript, deepgram_transcribe, create_transcript_manual
+from wudpecker_transcribe.celery_config import celery_app, create_transcript, get_transcript, deepgram_transcribe
 
 load_dotenv()
 
@@ -20,16 +20,10 @@ def root():
 async def create(request: Request):
     request_body = await request.body()
     request_data = json.loads(request_body)
-    uuid = request_data.get('uuid')
+    call_uuid = request_data.get('call_uuid')
     url = request_data.get('url')
-    lang = request_data.get('lang', 'NaN')
-    if lang == 'Nan':
-        task = create_transcript.delay(uuid, url)
-    else:
-        task = create_transcript_manual.delay(uuid, url, lang)
+    task = create_transcript.delay(call_uuid, url)
     return {"task_id": task.id}
-
-
     
 @app.post("/done")
 async def done(request: Request):
@@ -46,8 +40,8 @@ async def done(request: Request):
 async def deepgram_start(request: Request):
     request_body = await request.body()
     request_data = json.loads(request_body)
-    uuid = request_data.get('uuid')
+    call_uuid = request_data.get('call_uuid')
     url = request_data.get('url')
-    task = deepgram_transcribe.delay(uuid, url)
+    task = deepgram_transcribe.delay(call_uuid, url)
     return {"task_id": task.id}
     
